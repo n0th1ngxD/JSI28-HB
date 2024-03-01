c6be9dfc32b0ace21ca50cb41696ffeb2d5515fe
n0th1ngxD <vun9guy3n09@gmail.com>
2024-03-01 20:57:03 +0700

    10dd

--- CheckPoint1/B14/B14.js
+++ CheckPoint1/B14/B14.js
@@ -1,9 +1,10 @@
 class SinhVien {
-    constructor(sbd, hoten, diachi, mucuutien) {
+    constructor(sbd, hoten, diachi, mucuutien, khoithi) {
         this._sbd = sbd;
         this._hoten = hoten;
         this._diachi = diachi;
         this._mucuutien = mucuutien;
+        this._khoithi = khoithi;
     }
 }
 
@@ -12,15 +13,27 @@ class TuyenSinh {
         this._danhSach = danhSach;
     }
 
-    themThiSinh(sbd) {
+    themThiSinh(hocsinh) {
         // this._danhSach.push(sbd);
-        this._danhSach.push(SinhVien.hoten);
+        // this._danhSach.push(SinhVien._hoten);
+        let temp = {
+            sbd: hocsinh._sbd,
+            hoten: hocsinh._hoten,
+            diachi: hocsinh._diachi,
+            mucuutien: hocsinh._mucuutien,
+            khoithi: hocsinh._khoithi,
+        }
+        this._danhSach.push(temp);
+    }
+
+    show(hocsinh) {
+        return `Số báo danh: ${hocsinh._sbd}, Họ tên: ${hocsinh._hoten}, Địa chỉ: ${hocsinh._diachi}, Mức ưu tiên: ${hocsinh._mucuutien}, Khối thi: ${hocsinh._khoithi}`;
     }
 }
 
 let danhsach = [];
 let quanlidanhsanh = new TuyenSinh(danhsach);
 
-let Vu = new SinhVien(10062009, "Nguyen The Vu", "HH", "?");
+let Vu = new SinhVien(10062009, "Nguyen The Vu", "HH", "?", "A");
 quanlidanhsanh.themThiSinh(Vu);
 console.log(danhsach);
